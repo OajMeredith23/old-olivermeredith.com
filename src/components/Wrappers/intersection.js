9fcdb5fcb0639d593da78e4e42f9f8f450e8af77
OliverMeredith <mail@olivermeredith.com>
2019-08-20 14:18:40 +0100

    Revert "Fix layout inconsistencies"
    This reverts commit 79623cd1f57b721005f27c0f4dc0b7817c7bb39e.

--- src/components/Wrappers/intersection.js
+++ src/components/Wrappers/intersection.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import { useInView } from 'react-intersection-observer'
 import { css } from "@emotion/core"
-import { useSpring, animated } from 'react-spring'
+import {useSpring, animated} from 'react-spring'
 
 
-export default function Intersect(props) {
+export default function Intersect(props){
 
     const [ref, inView, entry] = useInView({
         threshold: 0,
@@ -14,12 +14,12 @@ export default function Intersect(props) {
 
     //Passes a prop of onScreen to child element, is true if on the screen
     const childWithProp = React.Children.map(props.children, (child) => {
-        return React.cloneElement(child, { onScreen: inView });
+        return React.cloneElement(child, {onScreen: inView});
     });
+    
 
-
-    return (
-
+    return(
+        
         <div ref={ref}
             css={
                 css`
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,21 +2,21 @@ import Typography from "typography"
 
 
 const options = {
-  googleFonts: [
-    {
-      name: "Playfair Display",
-      styles: ["700"],
-    },
-    {
-      name: "Fira Sans",
-      styles: ["400", "400i"],
-    },
-  ],
-  headerFontFamily: ['Playfair Display', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
-  bodyFontFamily: ['Fira Sans', 'sans-serif'],
-  headerWeight: 700,
-  bodyWeight: 400,
-}
+    googleFonts: [
+        {
+          name: "Playfair Display",
+          styles: ["400", "400i", "700"],
+        },
+        {
+          name: "Fira Sans",
+          styles: ["300", "400", "400i", "700", "700i"],
+        },
+      ],
+    headerFontFamily: ['Playfair Display', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+    bodyFontFamily: ['Fira Sans', 'serif'],
+    headerWeight: 700, 
+    bodyWeight: 400
+} 
 
 // console.log(kirkhamTheme)
 const typography = new Typography(options)
